42ccd2a6c331ace4bdf8ebe5c6a0551901f9fec7
luke-las <84208824+luke-las@users.noreply.github.com>
2022-02-17 11:57:45 -0500

    tags and formatting changes

--- wwwroot/js/Custom.js
+++ wwwroot/js/Custom.js
@@ -6,12 +6,12 @@ function AddTag() {
 
 
     //Lets use the new search function to help detect an error state
-    let searchResult = search(tagEntry.value);
+    let searchResult = searchDefects(tagEntry.value);
     if (searchResult != null) {
         //Trigger my sweet alert for whatever condition is contained in the searchResult var
 
         swalWithDarkButton.fire({
-            html: `<span class='font-weight-bolder'>${searchResult.toUpperCase()}</span>`,
+            html: `<span>${searchResult}</span>`,
         });
     }
     else {
@@ -69,14 +69,14 @@ function ReplaceTag(tag, index) {
 }
 
 
-function search(str) {
+function searchDefects(str) {
     if (str == "") {
         return 'Empty tags are not permitted'
     }
 
-    var tagsEl = document.getElementById('TagList');
-    if (tagsEl) {
-        let options = tagsEl.options;
+    var tagElemenets = document.getElementById('TagList');
+    if (tagElemenets) {
+        let options = tagElemenets.options;
         for (let index = 0; index < options.length; index++) {
             if (options[index].value == str)
                 return `The Tag #${str} was detected as a duplicate and is not permitted`
@@ -91,7 +91,9 @@ const swalWithDarkButton = Swal.mixin({
     customClass: {
         confirmButton: 'btn btn-danger btn-sm btn-block btn-outline-dark'
     },
-    imageUrl: '../assets/images/oops.png',
-    timer: 6000,
+    imageUrl: '/img/oopsimage.png',
+    /*timer: 6000,*/
+    imageWidth: 90,
+    imageHeight: 75,
     buttonsStyling: false
 });
